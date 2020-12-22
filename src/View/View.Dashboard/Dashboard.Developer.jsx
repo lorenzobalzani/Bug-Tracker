38940dcd50911875581b0e88dea8758445337ecd
lorenzobalzani <balzanilo@gmail.com>
2020-12-22 14:21:54 +0100

    Added priority graph in developer dashboard

--- src/View/View.Dashboard/Dashboard.Developer.jsx
+++ src/View/View.Dashboard/Dashboard.Developer.jsx
@@ -43,6 +43,7 @@ function DashboardDeveloper() {
     const { user, getAccessTokenSilently } = useAuth0();
     let [ statusGraph, setStatusGraph] = useState();
     let [ typeGraph, setTypeGraph] = useState();
+    let [ priorityGraph, setPriorityGraph] = useState();
 
     useEffect(() => {
         const ticketController = new TicketController();
@@ -56,10 +57,13 @@ function DashboardDeveloper() {
                 .then(response => {
                     setStatusGraph([]);
                     setTypeGraph([]);
+                    setPriorityGraph([]);
                     setStatusGraph(statusGraph => [...statusGraph, response.data.filter(ticket => ticket.status === "Open").length, 
                     response.data.filter(ticket => ticket.status === "In progress").length, response.data.filter(ticket => ticket.status === "Closed").length]);
                     setTypeGraph(typeGraph => [...typeGraph, response.data.filter(ticket => ticket.type === "Bug").length, 
                     response.data.filter(ticket => ticket.type === "Feature").length]);
+                    setPriorityGraph(priorityGraph => [...priorityGraph, response.data.filter(ticket => ticket.priority === "Low").length, 
+                    response.data.filter(ticket => ticket.priority === "Normal").length, response.data.filter(ticket => ticket.priority === "High").length]);
                 })
                  .catch(e => {
                    console.log(e);
@@ -71,16 +75,20 @@ function DashboardDeveloper() {
            getTickets();
     }, [getAccessTokenSilently, user])
 
-    return (<div className="col col-12">
-            <div className="col col-12">
+    return (<>
+            <div className="col col-12 col-xl-6">
                 <BarGraph title={"Ticket by status"} data={statusGraph} labelsX={['Open', 'In progress', 'Closed']} 
                 colors={['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(75, 192, 192, 0.2)']}/>
            </div>
-           <div className="col col-12">
+           <div className="col col-12 col-xl-6">
                 <BarGraph title={"Ticket by type"} data={typeGraph} labelsX={['Bug', 'Feature']} 
                         colors={['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)']}/>
            </div>
-    </div>);
+           <div className="col col-12">
+                <BarGraph title={"Ticket by priority"} data={priorityGraph} labelsX={['Low', 'Normal', 'High']} 
+                        colors={['rgba(75, 192, 192, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 99, 132, 0.2)']}/>
+           </div>
+    </>);
 }
 
 export default DashboardDeveloper;
