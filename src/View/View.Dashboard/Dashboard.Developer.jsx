57e097b384993d88d6dc202860b4c35366e1a4df
lorenzobalzani <balzanilo@gmail.com>
2020-12-22 14:15:46 +0100

    Now tickets graphs in Developer dashboard work

--- src/View/View.Dashboard/Dashboard.Developer.jsx
+++ src/View/View.Dashboard/Dashboard.Developer.jsx
@@ -1,25 +1,18 @@
-import {Bar } from 'react-chartjs-2'
+import { useEffect, useState } from 'react';
+import { Bar } from 'react-chartjs-2'
+import { useAuth0 } from "@auth0/auth0-react";
+import TicketController from '../../Controller/Ticket.Controller';
 
-function DashboardDeveloper() {
-    return (<div className="col col-12">
-        <div className="col col-12">
-            <Bar
+function BarGraph(props) {
+    return(
+        <Bar
             data={{
-                labels: ['Open', 'In progress', 'Closed'],
+                labels: props.labelsX,
                 datasets: [
                     {
-                        label: "Status of tickets",
-                        data: [3, 10, 3],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(75, 192, 192, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(75, 192, 192, 1)',
-                        ],
+                        data: props.data,
+                        backgroundColor: props.colors,
+                        borderColor: props.colors,
                         borderWidth: 1
                     }]
             }}
@@ -30,7 +23,7 @@ function DashboardDeveloper() {
                     },
                     title: {
                         display: true,
-                        text: 'Tickets by status'
+                        text: props.title
                     },
                     layout: {
                        
@@ -43,46 +36,49 @@ function DashboardDeveloper() {
                         }]
                     }
             }}/>
+    );
+}
+
+function DashboardDeveloper() {
+    const { user, getAccessTokenSilently } = useAuth0();
+    let [ statusGraph, setStatusGraph] = useState();
+    let [ typeGraph, setTypeGraph] = useState();
+
+    useEffect(() => {
+        const ticketController = new TicketController();
+        const getTickets = async () => {
+            try {
+               const token = await getAccessTokenSilently({
+                 permissions: "read:tickets"
+               });
+               ticketController.setAccessToken(token);
+               ticketController.getTicketsByDeveloperEmail(user.email)
+                .then(response => {
+                    setStatusGraph([]);
+                    setTypeGraph([]);
+                    setStatusGraph(statusGraph => [...statusGraph, response.data.filter(ticket => ticket.status === "Open").length, 
+                    response.data.filter(ticket => ticket.status === "In progress").length, response.data.filter(ticket => ticket.status === "Closed").length]);
+                    setTypeGraph(typeGraph => [...typeGraph, response.data.filter(ticket => ticket.type === "Bug").length, 
+                    response.data.filter(ticket => ticket.type === "Feature").length]);
+                })
+                 .catch(e => {
+                   console.log(e);
+                 });
+             } catch (e) {
+               console.log(e.message);
+             }
+           };
+           getTickets();
+    }, [getAccessTokenSilently, user])
+
+    return (<div className="col col-12">
+            <div className="col col-12">
+                <BarGraph title={"Ticket by status"} data={statusGraph} labelsX={['Open', 'In progress', 'Closed']} 
+                colors={['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(75, 192, 192, 0.2)']}/>
            </div>
            <div className="col col-12">
-           <Bar
-           data={{
-               labels: ['Bug', 'Feature'],
-               datasets: [
-                   {
-                       label: "Status of tickets",
-                       data: [3, 10, 3],
-                       backgroundColor: [
-                           'rgba(255, 99, 132, 0.2)',
-                           'rgba(54, 162, 235, 0.2)',
-                       ],
-                       borderColor: [
-                           'rgba(255, 99, 132, 1)',
-                           'rgba(54, 162, 235, 1)'
-                       ],
-                       borderWidth: 1
-                   }]
-           }}
-           options={{
-                   maintainAspectRatio: false,
-                   legend: {
-                       display: false,
-                   },
-                   title: {
-                       display: true,
-                       text: 'Tickets by type'
-                   },
-                   layout: {
-                      
-                   },
-                   scales: {
-                       yAxes : [{
-                           ticks: {
-                               beginAtZero: true
-                           }
-                       }]
-                   }
-           }}/>
+                <BarGraph title={"Ticket by type"} data={typeGraph} labelsX={['Bug', 'Feature']} 
+                        colors={['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)']}/>
            </div>
     </div>);
 }
--- src/View/View.Dashboard/Dashboard.jsx
+++ src/View/View.Dashboard/Dashboard.jsx
@@ -1,8 +1,5 @@
-import {Bar, Radar} from 'react-chartjs-2'
 import { useState, useEffect } from 'react'
 import { useAuth0 } from "@auth0/auth0-react";
-import ProjectController from '../../Controller/Project.Controller';
-import TicketController from '../../Controller/Ticket.Controller';
 import ConditionalRender from '../View.Utility/ConditionalRender';
 import DashboardDeveloper from './Dashboard.Developer';
 import DashboardProjectManager from './Dashboard.ProjectManager';
@@ -12,42 +9,15 @@ import jwt from 'jsonwebtoken';
 function Dashboard() {
     let data = [15, 3, 30];
     let data2 = [4, 15, 20];
-    let [ tickets, setTickets ] = useState([]);
+   
     let [ permissions, setPermissions ] = useState([]);
     const { user, getAccessTokenSilently } = useAuth0();
-    const projectController = new ProjectController();
-    const ticketController = new TicketController();
+    
 
     useEffect(() => {
         getAccessTokenSilently().then(token => {
                    setPermissions((jwt.decode(token)).permissions);
         })
-
-        const getTickets = async () => {
-         try {
-            const token = await getAccessTokenSilently({
-              permissions: "read:projects read:tickets"
-            });
-            projectController.setAccessToken(token);
-            ticketController.setAccessToken(token);
-            projectController.getProjectsByProjectManager(user.email)
-              .then(response => {
-                response.data.map(project => {
-                    console.log(project)
-                    ticketController.getTicketsByProjectId(project.id).
-                    then(response => {
-                        setTickets(response.data);
-                    })
-                })
-              })
-              .catch(e => {
-                console.log(e);
-              });
-          } catch (e) {
-            console.log(e.message);
-          }
-        };
-        getTickets();
       }, []);
 
     if (ConditionalRender(permissions, "read:projects")) {
