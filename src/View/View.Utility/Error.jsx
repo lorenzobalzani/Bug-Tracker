9564a4cfdcb646dd7864599d1c10c587d6ebab2d
lorenzobalzani <balzanilo@gmail.com>
2020-12-23 18:39:20 +0100

    Created error page when server is offline

---
+++ src/View/View.Utility/Error.jsx
@@ -0,0 +1,15 @@
+import '../Styles/Error.css';
+
+function Error(){
+    return (
+    <div class="error container">
+        <div className="row">
+            <div className="col col-12">
+             <h1>500 Server unreachable</h1>
+             <span>Sorry, server is unreachable. Try later ;)</span>
+            </div>
+        </div>
+    </div>);
+}
+
+export default Error
